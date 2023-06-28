dfa049405e46459e23c1affd085f7e61019197a8
Gavin <gavinsharrold@gmail.com>
2023-06-28 18:38:26 -0400

    blank screen is now blue

--- testingGraphics.cpp
+++ testingGraphics.cpp
@@ -5,6 +5,15 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -18,12 +27,32 @@ int main() {
 		return -1;
 	}
 
+	glfwMakeContextCurrent(window);
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		//input
+		processInput(window);
+
+		// rendering commands
+		glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+
+	glfwTerminate();
 	return 0;
 }
